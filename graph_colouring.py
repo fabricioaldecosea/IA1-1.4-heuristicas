1e80a02cc9cbdf67fde40c24c7c4c0902e87cca8
fabricioaldecosea <faldecosea19902@gmail.com>
2019-04-10 00:33:50 -0300

    Archivo que mando max

--- graph_colouring.py
+++ graph_colouring.py
@@ -26,54 +26,124 @@ chequear el sucesor a partir del choice - hacer choice de a nodo (de todo el res
 GRAPH_01 = [(0,1), (0,2), (1,2), (1,6), (2,4), (2,6), (3,5), (5,6)]
 
 def randomGraphEdges(nodeCount=10, edgeCount=30):
-	"""Returns a list of `edgeCount` edges selected at random ,for a graph of 
-	`nodeCount` nodes.
-	"""
-	allEdges = list(combinations(range(nodeCount), 2))
-	return sample(allEdges, min(len(allEdges), edgeCount))
+    """Returns a list of `edgeCount` edges selected at random ,for a graph of
+    `nodeCount` nodes.
+    """
+    allEdges = list(combinations(range(nodeCount), 2))
+    return sample(allEdges, min(len(allEdges), edgeCount))
+
+
+def nodes():
+    nodes = {}
+    for (x, y) in GRAPH_01:
+        nodes[x] = ""
+        nodes[y] = ""
+    # return nodes
+    return {0: 'R', 1: '', 2: 'B', 6: '', 4: 'G', 3: '', 5: ''}
+
 
 def randomColouring(nodes, colours='RGB'):
-	"""Returns a random assignment of `colours` to nodes, as a dict.
-	"""
-	return {n: choice(colours) for n in nodes}
-	
+    """Returns a random assignment of `colours` to nodes, as a dict.
+    """
+    return {n: choice(colours) for n in nodes}
+
+
 def nodesFromEdges(edges):
-	"""Given a list of graph `edges`, returns the set of all nodes connected 
-	by said edges.
-	"""
-	return set(sum(([x,y] for (x,y) in edges), []))
+    """Given a list of graph `edges`, returns the set of all nodes connected
+    by said edges.
+    """
+    return set(sum(([x, y] for (x, y) in edges), []))
+
 
 def adjacent(edges, node):
-	"""Returns the set of nodes connected to the given `node` according to the
-	given `edges`.
-	"""
-	return set(x if y == node else y for (x,y) in edges 
-		if x == node or y == node)
-	
+    """Returns the set of nodes connected to the given `node` according to the
+    given `edges`.
+    """
+    return set(x if y == node else y for (x, y) in edges
+               if x == node or y == node)
+
+
 def colourCollisions(edges, colours):
-	"""Returns the `edges` that connect nodes painted with the same colour, 
-	according to the `colours` dict.
-	"""
-	return set((x,y) for (x,y) in edges if colours[x] == colours[y])
+    """Returns the `edges` that connect nodes painted with the same colour,
+    according to the `colours` dict.
+    """
+    return set((x, y) for (x, y) in edges if colours[x] == colours[y])
+
 
 def randomSearch(edges, colours='RGB', tries=1000):
-	"""Resolve a graph colouring problem by random search.
-	"""
-	nodes = nodesFromEdges(edges)
-	for _ in range(tries):
-		colouring = randomColouring(nodes, colours)
-		collisions = colourCollisions(edges, colouring)
-		yield (colouring, collisions)
-		if not collisions:
-			break
+    """Resolve a graph colouring problem by random search.
+    """
+    nodes = nodesFromEdges(edges)
+    for _ in range(tries):
+        colouring = randomColouring(nodes, colours)
+        collisions = colourCollisions(edges, colouring)
+        yield (colouring, collisions)
+        if not collisions:
+            break
+
+
+def h1(colouring):
+    """Documentation.
+    """
+    edges = 0
+    for (x, y) in GRAPH_01:
+        if colouring[x] == '' and colouring[y] == '':
+            edges += 1
+    return edges
+
+
+def h2(colouring):
+    """Documentation.
+    """
+    edges = 0
+    for (x, y) in GRAPH_01:
+        if colouring[x] == '' or colouring[y] == '':
+            edges += 1
+    return edges
+
+
+def h3(colouring):
+    """Documentation.
+    """
+    edges = 0
+    for (x, y) in GRAPH_01:
+        if colouring[x] != '' and colouring[y] != '':
+            edges += 1
+    return edges
+
+
+def h4(colouring):
+    """Documentation.
+    """
+    nodes_colour = [0, 0, 0]
+    for (x, y) in colouring:
+        if colouring[y] == 'R':
+            nodes_colour[0] += 1
+        if colouring[y] == 'G':
+            nodes_colour[1] += 1
+        if colouring[y] == 'B':
+            nodes_colour[2] += 1
+    print(nodes_colour)
+
 
 if __name__ == '__main__':
-	print("Graph colouring with random search with graph %s" % GRAPH_01)
-	step = 1
-	for (colouring, collisions) in randomSearch(GRAPH_01):
-		print("\t%d: %s\t%d collisions found." % (step, colouring, len(collisions)))
-		step = step + 1
-	if collisions:
-		print("No solution found!")
-	else:
-		print("Solution: %s" % colouring)
+    nodes = nodes()
+
+    # print(h1(nodes))
+    # print(h2(nodes))
+    # print(h3(nodes))
+    print(h4(nodes))
+    # for i in range(len(nodes)):
+
+    # if (colourCollisions())
+
+    # print("Graph colouring with random search with graph %s" % GRAPH_01)
+    # step = 1
+    # for (colouring, collisions) in randomSearch(GRAPH_01):
+    #     print("\t%d: %s\t%d collisions found." %
+    #           (step, colouring, len(collisions)))
+    #     step = step + 1
+    # if collisions:
+    #     print("No solution found!")
+    # else:
+    #     print("Solution: %s" % colouring)
